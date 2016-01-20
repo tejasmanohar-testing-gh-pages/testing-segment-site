53a4ab6bfc5b7920eb4a6a55eceefcbd1fbd81e3
Tejas Manohar <me@tejas.io>
2016-01-19 16:04:30 -0800

    feat: error stufff for bugsnag

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+// causing errors to see data in bugsnag
+analytics.ready(function () {
+  x()
+})
+
 if (localStorage.getItem('email') && localStorage.getItem('password')) {
   alert('already logged-in!')
   analytics.track(
